16af869da40fcea4d9157bf54ced8373fdc2c9f3
crissx8 <carralerocristianx@gmail.com>
2025-09-05 21:34:15 -0400

    refactor: Move skills and services to json

--- src/locales/portfolio/meta-en.ts
+++
@@ -1,9 +0,0 @@
-export const KEYWORDS =
-	"portfolio, developer, full-stack, web development, programming, projects";
-
-export const DESCRIPTION =
-	"Portfolio of Cristian Carralero, Full Stack and Web3 developer specialized in React, Typescript, Rust and Solidity.";
-
-export const CANONICAL = "https://crisscode.xyz";
-
-export const OG_LOCALE = "en_US";
--- src/locales/portfolio/meta-es.ts
+++
@@ -1,9 +0,0 @@
-export const KEYWORDS =
-	"portafolio, desarrollador, full-stack, desarrollo web, programación, proyectos";
-
-export const DESCRIPTION =
-	"Portafolio de Cristian Carralero, desarrollador Full Stack y Web3 especializado en React, Typescript, Rust y Solidity.";
-
-export const CANONICAL = "https://crisscode.xyz/es";
-
-export const OG_LOCALE = "es_ES";
